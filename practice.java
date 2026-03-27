967c8bd7338759ebe02828c35d0a03331773d6a1
Pooja-27 <poojarm2797@gmail.com>
2026-03-27 21:41:02 +0530

    Type casting/conversion, operators and conditional statements

--- practice.java
+++ practice.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("Hello ");
-        System.out.print("World");
+        int day = 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
     }
 }
