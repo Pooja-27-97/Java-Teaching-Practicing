073a2a23aa474a0c75c80afe56b1f2af8e02d15e
Pooja-27 <poojarm2797@gmail.com>
2026-04-02 23:56:50 +0530

    Functions and Prime number

--- practice.java
+++ practice.java
@@ -1,30 +1,33 @@
 import java.util.*;
 
 public class practice {
-    public static void printHello() {
-        System.out.println("Hello!");
+    public static boolean checkPrime(int num) {
+        boolean isPrime = true;
+        if (num == 2) {
+            isPrime = true;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        return isPrime;
     }
 
-    public static void printSum(int a, int b) {
-        int result = a + b;
-        System.out.println("Sum: " + result);
-    }
-
-    public static int printSum(int a, int b, int c) {
-        int sum = (a + b + c);
-        return sum;
-    }
-
-    public static float printSum(float a, float b) {
-        return (a + b);
+    public static void printPrime(int num) {
+        for (int i = 2; i <= num; i++) {
+            if (checkPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        System.out.println("Created a function");
-        printHello();
-        printSum(10, 20);
-        System.out.println(printSum(100.24f, 20.49f));
-        int sum = printSum(5, 20, 10);
-        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        printPrime(num);
     }
 }
