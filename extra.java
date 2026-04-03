d2f298387b68ab18c9f83dcc21a006ff7c743775
Pooja-27 <poojarm2797@gmail.com>
2026-04-03 19:48:13 +0530

    Practice question

---
+++ extra.java
@@ -0,0 +1,6 @@
+public class extra {
+    public static void main(String args[]) {
+        double num = Math.floor(Math.random() * (30 - 20 + 1) + 20);
+        System.out.println((int) num);
+    }
+}
