af41a094ca8bd53cdfa70e73488f7b3e7ef12f0a
Pooja-27 <poojarm2797@gmail.com>
2026-04-03 21:44:33 +0530

    Arrays

--- practice.java
+++ practice.java
@@ -1,33 +1,21 @@
 import java.util.*;
 
 public class practice {
-    public static boolean checkPrime(int num) {
-        boolean isPrime = true;
-        if (num == 2) {
-            isPrime = true;
-        } else {
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        return isPrime;
-    }
-
-    public static void printPrime(int num) {
-        for (int i = 2; i <= num; i++) {
-            if (checkPrime(i)) {
-                System.out.print(i + " ");
-            }
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        printPrime(num);
+        int arr[] = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        printArray(arr);
     }
 }
