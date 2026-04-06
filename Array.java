d4d7a3187d57da1384cfa997d2ea65024f853060
Pooja-27 <poojarm2797@gmail.com>
2026-04-06 21:13:48 +0530

    Array operations

--- Array.java
+++ Array.java
@@ -21,10 +21,97 @@ public class Array {
         return small;
     }
 
-    public static void main(String args[]) {
-        int arr[] = { 4, 5, 8, 16, 25, 178, 102 };
+    public static int linearSearch(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-        System.out.println("Largest elt: " + printLargest(arr));
-        System.out.println("Smallest elt: " + printSmallest(arr));
+    public static int binarySearch(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == key)
+                return mid;
+            if (arr[mid] < key)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void reverse(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void printPairs(int arr[]) {
+        int pairs = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ") ");
+                pairs++;
+            }
+            System.out.println();
+        }
+        System.out.println("Pairs: " + pairs);
+    }
+
+    public static void printSubArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + ", ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 5, 8, 16, 25, 78, 102 };
+
+        // System.out.println("Largest elt: " + printLargest(arr));
+        // System.out.println("Smallest elt: " + printSmallest(arr));
+
+        // int index = linearSearch(arr, 18);
+        // if (index == -1) {
+        // System.out.println("Not found!");
+        // } else {
+        // System.out.println("Element fount at " + index);
+        // }
+
+        // int index = binarySearch(arr, 25);
+        // if (index == -1) {
+        // System.out.println("Not found!");
+        // } else {
+        // System.out.println("Element fount at " + index);
+        // }
+
+        // reverse(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
+        // System.out.println();
+
+        // printPairs(arr);
+        printSubArray(arr);
     }
 }
