4763a5511ca0b32f3bbce43536ebdaf76f405541
Pooja-27 <poojarm2797@gmail.com>
2026-04-09 21:42:44 +0530

    Stock problem

--- BuyAndSellStocks.java
+++ BuyAndSellStocks.java
@@ -1,5 +1,23 @@
 public class BuyAndSellStocks {
-    public static void main(String[] args) {
+    public static void CalcProfit(int price[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int day = 0;
 
+        for (int i = 0; i < price.length; i++) {
+            if (price[i] > buyPrice) {
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+                day = i;
+            } else {
+                buyPrice = price[i];
+            }
+        }
+        System.out.println("Max profit: " + maxProfit + " on day " + day);
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        CalcProfit(prices);
     }
 }
